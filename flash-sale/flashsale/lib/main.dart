309757df87773368dc0c4d8b7571309166a27bf8
HanzxRYU <rafi0315@abudzar.sch.id>
2025-12-21 21:15:59 +0700

    up

--- flash-sale/flashsale/lib/main.dart
+++ flash-sale/flashsale/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flashsale/flashsale.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -16,28 +15,6 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: Flashsale(),
-    );
-  }
-}
-
-class Latihan extends StatelessWidget {
-  const Latihan({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Latihan Flutter')),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          height: 300,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: Colors.amber,
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-      ),
     );
   }
 }
